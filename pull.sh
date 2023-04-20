6df2554ce2d0054302026342750104bdaaca0cb5
Oxygen! <129119112+IneedOxygen@users.noreply.github.com>
2023-04-20 22:37:49 +0700

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull registry.gitlab.com/tenten8401/pterodactyl-nginx
+docker pull ghcr.io/parkervcp/yolks:java_17
+docker pull ghcr.io/parkervcp/yolks:debian
+docker pull danbothosting/aio
